a4b97f1c2d3b12f063e9e573ee11b874351bdffd
tomas99batista <tomas99batista@gmail.com>
2019-11-05 04:11:11 +0000

    pqp django

--- jobber/job_app/models.py
+++ jobber/job_app/models.py
@@ -47,11 +47,8 @@ class Emprego(models.Model):
     publisher = models.ForeignKey(Empresa, on_delete=models.CASCADE)
     location = models.IntegerField(choices=LOCATION)  # Pode ser Remote, no Local da empresa ou noutra sede ou sth else
     # Professional Details
-    job_sector = models.IntegerField(choices=JOB_SECTOR, max_length=50)  # IT, Economy
-    job_sector = models.CharField(choices=JOB_SECTOR, max_length=50)  # IT, Economy
-    experience_level = models.IntegerField(choices=EXPERIENCE_LEVEL, default=1)
-    location = models.IntegerField(choices=LOCATION)    # Professional Details
     job_sector = models.IntegerField(choices=JOB_SECTOR)  # IT, Economy
+    location = models.IntegerField(choices=LOCATION)    # Professional Details
     file = models.FileField(blank=True)  # Can post a PDF with more details of the job, but it's totally optional
 
     def __str__(self):
